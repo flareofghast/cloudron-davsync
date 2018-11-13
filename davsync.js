3e2df2d578529670c81eb7800f18285c4742ee7a
Johannes Zellner <johannes@nebulon.de>
2018-11-13 21:17:02 +0100

    Add verify command

--- davsync.js
+++ davsync.js
@@ -5,24 +5,6 @@ var async = require('async'),
     ProgressBar = require('progress'),
     webdav = require('webdav');
 
-commander
-    .option('--source <url>', 'Source - full URL to addressbook or calendar')
-    .option('--source-username <username>', 'Source username')
-    .option('--source-password <password>', 'Source password')
-    .option('--destination <url>', 'Destination - full URL to addressbook or calendar')
-    .option('--destination-username <username>', 'Destination username')
-    .option('--destination-password <password>', 'Destination password')
-    .parse(process.argv);
-
-// We require all arguments
-if (!commander.source ||
-    !commander.sourceUsername ||
-    !commander.sourcePassword ||
-    !commander.destination ||
-    !commander.destinationUsername ||
-    !commander.destinationPassword
-) commander.help();
-
 function getContents(client, callback) {
     client.getDirectoryContents('/').then(function (result) {
         callback(null, result);
@@ -47,61 +29,124 @@ function putFile(client, filename, content, callback) {
     });
 }
 
-var srcClient = webdav(commander.source, commander.sourceUsername, commander.sourcePassword);
-var dstClient = webdav(commander.destination, commander.destinationUsername, commander.destinationPassword);
+function handleError(message, error) {
+    if (error.status === 401) return console.error(message, 'Invalid username or password.');
+    console.error(message, error.message);
+}
 
-var failedContents = [];
-var invalidContents = [];
+function sync(options) {
+    // We require all arguments
+    if (!options.source ||
+        !options.sourceUsername ||
+        !options.sourcePassword ||
+        !options.destination ||
+        !options.destinationUsername ||
+        !options.destinationPassword
+    ) return options.help();
 
-getContents(srcClient, function (error, srcContents) {
-    if (error) return console.error('Unable to list items on source server.', error);
+    var srcClient = webdav(options.source, options.sourceUsername, options.sourcePassword);
+    var dstClient = webdav(options.destination, options.destinationUsername, options.destinationPassword);
 
-    getContents(dstClient, function (error, dstContents) {
-        if (error) return console.error('Unable to list items on destination server.', error);
+    var failedContents = [];
+    var invalidContents = [];
 
-        console.log(`Found ${srcContents.length} items on source server.`);
-        console.log(`Found ${dstContents.length} items on destination server.`);
+    getContents(srcClient, function (error, srcContents) {
+        if (error) return handleError('Unable to list items on source server.', error);
 
-        var bar = new ProgressBar('Syncing :current/:total [:bar] :etas', { head: '>', total: srcContents.length });
+        getContents(dstClient, function (error, dstContents) {
+            if (error) return handleError('Unable to list items on destination server.', error);
 
-        async.eachLimit(srcContents, 10, function (srcContent, callback) {
-            getFile(srcClient, srcContent.filename, function (error, srcData) {
-                if (error) return callback(error);
+            console.log(`Found ${srcContents.length} items on source server.`);
+            console.log(`Found ${dstContents.length} items on destination server.`);
 
-                putFile(dstClient, srcContent.filename, srcData, function (error, result) {
-                    if (error && error.message.indexOf('415') !== -1) invalidContents.push({ content: srcContent, data: srcData.toString() });
-                    else if (error) failedContents.push({ content: srcContent, data: srcData.toString(), error: error });
+            var bar = new ProgressBar('Syncing :current/:total [:bar] :etas', { head: '>', total: srcContents.length });
 
-                    bar.tick();
+            async.eachLimit(srcContents, 10, function (srcContent, callback) {
+                getFile(srcClient, srcContent.filename, function (error, srcData) {
+                    if (error) return callback(error);
 
-                    callback();
+                    putFile(dstClient, srcContent.filename, srcData, function (error, result) {
+                        if (error && error.message.indexOf('415') !== -1) invalidContents.push({ content: srcContent, data: srcData.toString() });
+                        else if (error) failedContents.push({ content: srcContent, data: srcData.toString(), errorMessage: error.message });
+
+                        bar.tick();
+
+                        callback();
+                    });
                 });
+            }, function (error) {
+                if (error) return handleError('Failed to sync.', error);
+
+                if (failedContents.length) {
+                    console.error('The following items failed to sync:');
+                    failedContents.forEach(function (f) {
+                        console.log('===============================');
+                        console.log(f.content.filename);
+                        console.log(f.data);
+                        console.log(f.errorMessage);
+                    });
+                }
+
+                if (invalidContents.length) {
+                    console.error('The following items were invalid and not synced:');
+                    invalidContents.forEach(function (f) {
+                        console.log('===============================');
+                        console.log(f.content.filename);
+                        console.log(f.data);
+                    });
+                }
+
+                console.log(`Done. ${failedContents.length} failed. ${invalidContents.length} invalid.`);
+
+                process.exit((failedContents.length || invalidContents.length) ? 1 : 0);
             });
-        }, function (error) {
-            if (error) return console.error('Failed to sync.', error);
-
-            if (failedContents.length) {
-                console.error('The following items failed to sync:');
-                failedContents.forEach(function (f) {
-                    console.log('===============================');
-                    console.log(f.content.filename);
-                    console.log(f.data);
-                    console.log(f.error);
-                });
-            }
-
-            if (invalidContents.length) {
-                console.error('The following items were invalid and not synced:');
-                invalidContents.forEach(function (f) {
-                    console.log('===============================');
-                    console.log(f.content.filename);
-                    console.log(f.data);
-                });
-            }
-
-            console.log(`Done. ${failedContents.length} failed. ${invalidContents.length} invalid.`);
-
-            process.exit((failedContents.length || invalidContents.length) ? 1 : 0);
         });
     });
-});
+}
+
+function verify(options) {
+    // We require all arguments
+    if (!options.source ||
+        !options.sourceUsername ||
+        !options.sourcePassword
+    ) return options.help();
+
+    var client = webdav(options.source, options.sourceUsername, options.sourcePassword);
+
+    getContents(client, function (error, items) {
+        if (error) return handleError('Unable to list items on source server.', error);
+
+        console.log(`Found ${items.length} items on source server.`);
+
+        // empty resources are ok
+        if(items.length === 0) return console.log('Successfully verified');
+
+        getFile(client, items[0].filename, function (error, data) {
+            if (error) return handleError('Unable to get first item on source server.', error);
+            console.log('Successfully verified');
+        });
+    });
+}
+
+commander.command('sync')
+    .description('Sync DAV resources between servers')
+    .option('--source <url>', 'Source - full DAV URL to addressbook or calendar')
+    .option('--source-username <username>', 'Source username')
+    .option('--source-password <password>', 'Source password')
+    .option('--destination <url>', 'Destination - full DAV URL to addressbook or calendar')
+    .option('--destination-username <username>', 'Destination username')
+    .option('--destination-password <password>', 'Destination password')
+    .action(sync);
+
+commander.command('verify')
+    .description('Verify DAV source')
+    .option('--source <url>', 'Source - full DAV URL to addressbook or calendar')
+    .option('--source-username <username>', 'Source username')
+    .option('--source-password <password>', 'Source password')
+    .action(verify);
+
+// error on unknown commands or no command
+commander.on('command:*', function () { commander.help(); });
+if (!process.argv.slice(2).length) return commander.help();
+
+commander.parse(process.argv);
